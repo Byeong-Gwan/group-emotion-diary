0bb1ccc5b31b06e41b821dd252213df84f506152
chan8632 <duscks8632@gmail.com>
2025-11-19 17:07:20 +0900

    feat: [layout] 헤더 날짜 선택 기능 추가 (react-datepicker 라이브러리 적용)

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,23 +1,40 @@
- import { Container, Nav, Navbar } from 'react-bootstrap'
- import { Link, Outlet } from 'react-router-dom'
-import LoginPage from '../pages/auth/LoginPage'
-
- export default function RootLayout() {
-   return (
-     <>
-       <Navbar bg="light" expand="lg">
-         <Container>
-           <Navbar.Brand as={Link} to="/">Emotion Diary</Navbar.Brand>
-           <Nav className="ms-auto">
-            <LoginPage />
-             <Nav.Link as={Link} to="/">Diaries</Nav.Link>
-             <Nav.Link as={Link} to="/diary/new">New</Nav.Link>
-           </Nav>
-         </Container>
-       </Navbar>
-       <Container className="py-4">
-         <Outlet />
-       </Container>
-     </>
-   )
- }
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link, Outlet } from "react-router-dom";
+import LoginPage from "../pages/auth/LoginPage";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+export default function RootLayout() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            Emotion Diary
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-center"
+          >
+            <Nav className="mx-auto">
+              <DatePicker
+                dateFormat="yyyy/MM/dd"
+                selected={selectedDate}
+                onChange={(date) => setSelectedDate(date)}
+                className="form-control text-center"
+              />
+            </Nav>
+            <Nav>
+              <LoginPage />
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container className="py-4">
+        <Outlet />
+      </Container>
+    </>
+  );
+}
