9a3fbeac3d5c73ca0072140026797b6599209d45
chan8632 <duscks8632@gmail.com>
2025-11-19 19:36:44 +0900

    feat: [layout] 헤더 네비게이션 정렬 수정 및 목록 가기 버튼 추가(조건부 렌더링) - 일기 수정,생성 페이지, 상세페이지에서 다이어리 목록 이라는 버튼 생성

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,11 +1,13 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
-import { Link, Outlet } from "react-router-dom";
-import LoginPage from "../pages/auth/LoginPage";
 import { useState } from "react";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Link, Outlet, useLocation } from "react-router-dom";
+import LoginPage from "../pages/auth/LoginPage";
 export default function RootLayout() {
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const location = useLocation().pathname;
+
   return (
     <>
       <Navbar bg="light" expand="lg">
@@ -16,7 +18,7 @@ export default function RootLayout() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse
             id="basic-navbar-nav"
-            className="justify-content-center"
+            className="justify-content-center d-flex flex-column gap-3"
           >
             <Nav className="mx-auto">
               <DatePicker
@@ -26,7 +28,17 @@ export default function RootLayout() {
                 className="form-control text-center"
               />
             </Nav>
-            <Nav>
+            <Nav className="justify-content-center d-flex flex-column gap-3">
+              {location === "/" ? null : (
+                <Button
+                  as={Link}
+                  to="/"
+                  className="me-md-4 "
+                  variant="outline-primary"
+                >
+                  📚다이어리 목록
+                </Button>
+              )}
               <LoginPage />
             </Nav>
           </Navbar.Collapse>
