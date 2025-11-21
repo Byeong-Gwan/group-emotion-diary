4fe6de234147e0d167cff62664a2b9800333932a
JJH <jeww1234@gmail.com>
2025-11-21 21:36:38 +0900

    dors text 수정

--- src/pages/diary/diary-List/DiaryListPage.jsx
+++ src/pages/diary/diary-List/DiaryListPage.jsx
@@ -167,7 +167,7 @@ export default function DiaryListPage() {
   return (
     <>
       <div className="d-flex justify-content-between align-items-center mb-3">
-        <h3 className="m-0">{selectedMonth}월 Diaries</h3>
+        <h3 className="m-0">{selectedMonth}월 Logs</h3>
         {userInfo ? (
           <Button as={Link} to="/diary/new">
             New
